cc9c0b3253ef01082c9193bc8d5fe44c38b653eb
parasharamit <amitparashar7@gmail.com>
2023-10-01 12:48:22 +0530

    striver

---
+++ google_minion/minion-work-assignment/solution1.py
@@ -0,0 +1,28 @@
+def solution(data, n):
+    count_mp = {}
+    size = len(data)
+    complain = set()
+
+    if n == 0:
+        return Nonenlp
+
+    for i in range(size):
+        if data[i] not in count_mp.keys():
+            count_mp[data[i]] = 1
+        else:
+            count_mp[data[i]] += 1
+            if count_mp[data[i]] > n:
+                complain.add(data[i])
+    data_new = []
+    for e in data:
+        if e not in complain:
+            data_new.append(e)
+
+    print(data_new)                
+
+    return data
+
+if __name__ == "__main__":
+    data = [1, 2, 2, 3, 3, 3, 4, 5, 5] 
+    n = 0
+    print(solution(data, n))
